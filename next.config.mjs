912fa8ef65ecc04c1967b47ae5f26c5fa828c98c
Kavindu Udara <karunasinghesampath38@gmail.com>
2025-02-11 01:47:52 +0530

    added asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/me'
+    basePath: '/me',
+  assetPrefix: '/me/'
 };
 
 export default nextConfig;
