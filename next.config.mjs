9d07e6ee91d56c8bf7cd59d21e9cba03a9fe2284
Kavindu Udara <karunasinghesampath38@gmail.com>
2025-02-11 09:02:18 +0530

    update deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/me',
-  assetPrefix: '/me/'
+  output: 'export',
+  basePath: '/me',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
